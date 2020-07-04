d4906b55d203d50da9d1436432b0d81ad3ded766
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-05 00:34:09 +0200

    v0.0.8: workarounds/systemd_quick_shutdown

---
+++ src/workarounds/systemd_quick_shutdown.mjs
@@ -0,0 +1,14 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+export default async(bun) => {
+  bun.needs('admFile', {
+    path: '/etc/systemd/system.conf.d/quick_shutdown.conf',
+    mimeType: 'static_ini',
+    content: {
+      // https://www.freedesktop.org/software/systemd/man/systemd-system.conf.html
+      Manager: {
+        DefaultTimeoutStopSec: 30,
+      },
+    },
+  });
+};
