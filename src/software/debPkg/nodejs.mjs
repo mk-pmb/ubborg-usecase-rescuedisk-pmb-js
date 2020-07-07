fa9113faf36d6432cfcfa3b08533903dfe0f6402
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-07 04:24:04 +0200

    v0.0.12: Add Node.js deb repo key

--- src/software/debPkg/nodejs.mjs
+++ src/software/debPkg/nodejs.mjs
@@ -2,6 +2,7 @@
 
 import mustBe from 'typechecks-pmb/must-be';
 import lPad from 'lodash.padstart';
+import loPick from 'lodash.pick';
 
 const nodeSourceBase = 'https://deb.nodesource.com/';
 const dfNodeOpts = {
@@ -12,6 +13,21 @@ const dfNodeOpts = {
     // where D is a digit 1-9 to indicate how many digits wide
     // the number shall be, adding leading zeroes if necessary.
     components: ['main'],
+
+    trustedLocalAptKeyRingName: 'nodesource',
+    // ^- For now you'll need a pre-made, already-converted
+    //    /etc/apt/trusted.gpg.d/nodesource.gpg that we can just copy.
+    keyUrl: nodeSourceBase + 'gpgkey/nodesource.gpg.key',
+    keyVerify: {
+      summary: [
+        'pub  4096R/68576280 2014-06-13 NodeSource <gpg@nodesource.com>',
+        'sub  4096R/AA01DA2C 2014-06-13',
+      ],
+      sha512hex: [
+        'fbd658af32f3c6d938b3ce5f126b8103a87f3ec871a986744a1d420b72cb56f7',
+        '2228417c6ba4bbe07bf31e5a2b3b4e0611cdbebd989beeb2fa942dcb06fd5a18',
+      ],
+    },
   },
 };
 
@@ -19,7 +35,9 @@ const dfNodeOpts = {
 function makeVersionTemplateRenderer(v, mustRepo) {
   function ins(m, w) { return m && lPad(v, +w); }
   const f = function versionTmpl(s) { return s && s.replace(/%(\d)v/g, ins); };
-  return f;
+  return Object.assign(f, {
+    prop(k) { return { [k]: f(mustRepo('nonEmpty str', k)) }; },
+  });
 }
 
 
@@ -36,6 +54,11 @@ async function installNodejs(bun) {
     name: 'nodejs',
     debUrls: [verTpl(mustRepo('nonEmpty str', 'debUrl'))],
     components: mustRepo('nonEmpty ary', 'components'),
+    ...verTpl.prop('trustedLocalAptKeyRingName'),
+    ...verTpl.prop('keyUrl'),
+    ...loPick(repoInfo, [
+      'keyVerify',
+    ]),
   });
 
   await bun.needs('debPkg', [
