56bd568904fb17331b8f61460bc5f4d8cce9060a
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-07 20:11:09 +0200

    v0.0.13: Support relative keyUrl for Node.js

--- src/software/debPkg/nodejs.mjs
+++ src/software/debPkg/nodejs.mjs
@@ -1,14 +1,15 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import nodeUrlLib from 'url';
+
 import mustBe from 'typechecks-pmb/must-be';
 import lPad from 'lodash.padstart';
 import loPick from 'lodash.pick';
 
-const nodeSourceBase = 'https://deb.nodesource.com/';
 const dfNodeOpts = {
   version: 12,
   repo: {
-    debUrl: nodeSourceBase + 'node_%1v.x/',
+    debUrl: 'https://deb.nodesource.com/node_%1v.x/',
     // %Dv is a placeholder for the version number,
     // where D is a digit 1-9 to indicate how many digits wide
     // the number shall be, adding leading zeroes if necessary.
@@ -17,7 +18,7 @@ const dfNodeOpts = {
     trustedLocalAptKeyRingName: 'nodesource',
     // ^- For now you'll need a pre-made, already-converted
     //    /etc/apt/trusted.gpg.d/nodesource.gpg that we can just copy.
-    keyUrl: nodeSourceBase + 'gpgkey/nodesource.gpg.key',
+    keyUrl: '/gpgkey/nodesource.gpg.key',
     keyVerify: {
       summary: [
         'pub  4096R/68576280 2014-06-13 NodeSource <gpg@nodesource.com>',
@@ -40,22 +41,28 @@ function makeVersionTemplateRenderer(v, mustRepo) {
   });
 }
 
+function urlResolveHref(h, b) { return String(new nodeUrlLib.URL(h, b)); }
+
 
 async function installNodejs(bun) {
-  const mustNode = mustBe.tProp('Node.js setting ',
-    bun.makeParamPopper().mustBe('tru | dictObj', 'nodejs'));
+  const nodeSett = bun.makeParamPopper().mustBe('tru | dictObj', 'nodejs');
+  const mustNode = mustBe.tProp('Node.js setting ', nodeSett);
   const repoInfo = mustNode('undef | dictObj', 'repo');
   const mustRepo = mustBe.tProp('Node.js debian repo setting ', repoInfo);
 
   const version = mustNode('pos int', 'version');
   const verTpl = makeVersionTemplateRenderer(version, mustRepo);
+  const debUrl = verTpl(mustRepo('nonEmpty str', 'debUrl'));
+
+  let keyUrl = mustRepo('undef | nul | nonEmpty str', 'keyUrl');
+  if (keyUrl) { keyUrl = urlResolveHref(verTpl(keyUrl), debUrl); }
 
   await bun.needs('debPkgRepo', {
     name: 'nodejs',
-    debUrls: [verTpl(mustRepo('nonEmpty str', 'debUrl'))],
+    debUrls: [debUrl],
     components: mustRepo('nonEmpty ary', 'components'),
     ...verTpl.prop('trustedLocalAptKeyRingName'),
-    ...verTpl.prop('keyUrl'),
+    keyUrl,
     ...loPick(repoInfo, [
       'keyVerify',
     ]),
