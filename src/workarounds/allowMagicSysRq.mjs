224b577d16db242c8166f2d22e22aa63fab348e3
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-23 19:33:55 +0200

    allowMagicSysRq

---
+++ src/workarounds/allowMagicSysRq.mjs
@@ -0,0 +1,16 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+export default async(bun) => {
+  // Security concerns described here:
+  // https://bugs.launchpad.net/ubuntu/+source/linux/+bug/194676
+  // However, session-killing and OOM kill can be really handy at times.
+
+  // German keyboards: https://de.wikipedia.org/wiki/Magische_S-Abf-Taste
+  // "Auf Tastaturen für Deutschland drückt man […] Alt + Druck + Taste […]"
+
+  bun.needs('admFile', {
+    path: '/etc/sysctl.d/90-allow-magic-sysrq.conf',
+    mimeType: 'text/plain',
+    content: 'kernel.sysrq = 1\n',
+  });
+};
