46749c899b1275815940647e124942b0e6cc0b89
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-25 22:05:39 +0200

    v0.0.23: Fix apt package names

--- src/software/debPkg/iconsGnomeDefault.mjs
+++ src/software/debPkg/iconsGnomeDefault.mjs
@@ -3,8 +3,11 @@
 export default async(bun) => {
   bun.needs('debPkg', [
     'gnome-icon-theme',
-    'gnome-icon-theme-symbolic',
     'gnome-icon-theme-suede',
+
+    // 'gnome-icon-theme-symbolic',
+    // focal apt: Note, selecting 'adwaita-icon-theme-full'
+    //            instead of 'gnome-icon-theme-symbolic'                                                                |
     'adwaita-icon-theme-full',
   ]);
 };
