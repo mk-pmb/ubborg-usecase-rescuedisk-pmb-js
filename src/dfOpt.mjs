980c69dde89fe2b8d7a0a657334833952f4769cd
mk-pmb <mk-pmb@users.noreply.github.com>
2020-08-01 18:01:37 +0200

    Move defaultKeyboard to cfg/primaryKeyboard

--- src/dfOpt.mjs
+++ src/dfOpt.mjs
@@ -1,7 +1,8 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 const defaultOpts = {
-  primaryKeyboard: {},    // see src/defaultKeyboard.mjs; or false to skip.
+  primaryKeyboard: {},    // see src/cfg/defaultKeyboard.mjs; or false to skip.
+  i18nLocales: {},        // see src/cfg/i18nLocales.mjs
   rescueUser: {},         // see src/rescueUser.mjs.
   workarounds: {},
   software_debPkg: {},
