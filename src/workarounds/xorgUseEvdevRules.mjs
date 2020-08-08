3bbbcec7ad4cc885211f7871370483bbaf577e66
mk-pmb <mk-pmb@users.noreply.github.com>
2020-08-08 06:18:01 +0200

    Enable evdev rules to fix keyboard layout

---
+++ src/workarounds/xorgUseEvdevRules.mjs
@@ -0,0 +1,20 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import fileGeneratedHint from '../fileGeneratedHint';
+
+export default async(bun) => {
+  bun.needs('admFile', {
+    pathPre: '/usr/share/X11/xorg.conf.d/',
+    path: '20-workaround-focal-xkb-use-evdev-rules.conf',
+    mimeType: 'lines',
+    content: [
+      fileGeneratedHint('# ', '\n'),
+      'Section "InputClass"',
+      '  Identifier "Enable evdev rules even on Ubuntu focal"',
+      '  MatchIsKeyboard "on"',
+      '  MatchDevicePath "/dev/input/event*"',
+      '  Option "XkbRules" "evdev"',
+      'EndSection',
+    ],
+  });
+};
