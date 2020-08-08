82b3843f78f7e8130487bdaa54167f2fd72c588b
mk-pmb <mk-pmb@users.noreply.github.com>
2020-08-08 06:18:32 +0200

    v0.0.30: Install remmina plugins

--- src/software/debPkg/remoteDesktop.mjs
+++ src/software/debPkg/remoteDesktop.mjs
@@ -1,8 +1,20 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+const remminaPlugins = [
+  'nx',
+  'rdp',
+  'secret',
+  'spice',
+  'vnc',
+  'www',
+  'xdmcp',
+].map(n => 'remmina-plugin-' + n);
+
 export default async(bun) => {
   bun.needs('debPkg', [
     'x11vnc',
+
     'remmina',
+    ...remminaPlugins,
   ]);
 };
