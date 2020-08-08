4d98fdc845281ce67c45412065e193278fb06879
mk-pmb <mk-pmb@users.noreply.github.com>
2020-08-08 06:18:01 +0200

    Enable evdev rules to fix keyboard layout

--- src/software/debPkg/xorgBasics.mjs
+++ src/software/debPkg/xorgBasics.mjs
@@ -6,13 +6,26 @@ const xGreeters = [
   'unity-greeter',
 ];
 
+const xinputDevices = [
+  'xserver-xorg-input-all',
+  // ^-- But in focal it seems to be missing some crucial dependencies, so…
+
+  'xserver-xorg-input-evdev',
+  'xserver-xorg-input-joystick',
+  // virtual package in focal // 'xserver-xorg-input-keyboard',
+  'xserver-xorg-input-mouse',
+  'xserver-xorg-input-wacom',
+];
+
 export default async(bun) => {
   bun.needs('debPkg', [
     ...xGreeters,
+    ...xinputDevices,
 
     'x11-xserver-utils',    // provides xsetroot
-    'xinput',
+    'xdiagnose',
     'xdotool',
+    'xinput',
     'xserver-xorg',
 
     'xsel',
