056dec00a6e173ad3402018641a38a4db0ee8114
mk-pmb <mk-pmb@users.noreply.github.com>
2020-08-14 19:21:19 +0200

    v0.0.32: Add [lms]trace and xterm

--- src/software/debPkg/debugUtil.mjs
+++ src/software/debPkg/debugUtil.mjs
@@ -1,18 +1,25 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+const straceAndFriends = [
+  'libc-dev-bin => cmd:mtrace',
+  'ltrace',
+  'strace',
+];
+
 export default async(bun) => {
   bun.needs('debPkg', [
+    ...straceAndFriends,
     'acpi',
-    // ¬focal 'dconf-tools',    // <-- dconf != debconf; includes dconf-editor
     'cpuid',
+    // ¬focal 'dconf-tools',    // <-- dconf != debconf; includes dconf-editor
     'debconf-utils',
     'hddtemp',
     'htop',
     'iotop',
     'lshw',
     'lsof',
-    'powertop',
     'mmc-utils',  // debug memory card slots
+    'powertop',
     'units',      // convert disk size prefixes
     'usbutils',   // lsusb
     // ¬focal 'winpdb',
