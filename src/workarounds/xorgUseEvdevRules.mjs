2cff0e72f0c556cee64cd41777f8183335cd879b
mk-pmb <mk-pmb@users.noreply.github.com>
2020-10-03 04:39:38 +0200

    Upgrade to xorgConfigDropFile

--- src/workarounds/xorgUseEvdevRules.mjs
+++ src/workarounds/xorgUseEvdevRules.mjs
@@ -1,20 +1,14 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import fileGeneratedHint from '../fileGeneratedHint';
-
 export default async(bun) => {
-  bun.needs('admFile', {
-    pathPre: '/usr/share/X11/xorg.conf.d/',
-    path: '20-workaround-focal-xkb-use-evdev-rules.conf',
-    mimeType: 'lines',
-    content: [
-      fileGeneratedHint('# ', '\n'),
-      'Section "InputClass"',
-      '  Identifier "Enable evdev rules even on Ubuntu focal"',
-      '  MatchIsKeyboard "yes"',
-      '  MatchDevicePath "/dev/input/event*"',
-      '  Option "XkbRules" "evdev"',
-      'EndSection',
+  bun.needs('xorgConfigDropFile', {
+    path: '20-workaround-focal-xkb-use-evdev-rules',
+    section: [
+      'InputClass',
+      'Identifier "Enable evdev rules even on Ubuntu focal"',
+      'MatchIsKeyboard "yes"',
+      'MatchDevicePath "/dev/input/event*"',
+      'Option "XkbRules" "evdev"',
     ],
   });
 };
