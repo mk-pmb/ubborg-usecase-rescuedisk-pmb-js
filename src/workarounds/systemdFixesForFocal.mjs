211f668e9490c61b5278ec62dabe37e1f2091f8f
mk-pmb <mk-pmb@users.noreply.github.com>
2021-01-06 07:30:08 +0100

    v0.0.41: workarounds/systemdFixesForFocal

---
+++ src/workarounds/systemdFixesForFocal.mjs
@@ -0,0 +1,10 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import sysdDropIn from 'ubborg-sysd-dropin-pmb';
+
+export default async(bun) => {
+  bun.needs('admFile', [
+    sysdDropIn('lib:dbus.socket', 'modernize_for_ubuntu_focal',
+      { Socket: { ListenStream: '/run/dbus/system_bus_socket' } }),
+  ]);
+};
