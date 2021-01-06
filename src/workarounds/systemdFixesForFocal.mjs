6b4ab6e5eee88f2983697846755e91c57fdf1865
mk-pmb <mk-pmb@users.noreply.github.com>
2021-01-06 07:44:31 +0100

    v0.0.42: workarounds/systemdFixesForFocal: disable old socket

--- src/workarounds/systemdFixesForFocal.mjs
+++ src/workarounds/systemdFixesForFocal.mjs
@@ -5,6 +5,6 @@ import sysdDropIn from 'ubborg-sysd-dropin-pmb';
 export default async(bun) => {
   bun.needs('admFile', [
     sysdDropIn('lib:dbus.socket', 'modernize_for_ubuntu_focal',
-      { Socket: { ListenStream: '/run/dbus/system_bus_socket' } }),
+      { Socket: { ListenStream: ['', '/run/dbus/system_bus_socket'] } }),
   ]);
 };
