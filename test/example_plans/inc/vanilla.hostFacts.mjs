feb3b729526291d6c933eb099fdc41cc860b3164
mk-pmb <mk-pmb@users.noreply.github.com>
2021-02-10 03:29:20 +0100

    [test] Require osVersion early, so it shakes up results less

---
+++ test/example_plans/inc/vanilla.hostFacts.mjs
@@ -0,0 +1,11 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+const hf = {};
+
+hf.osVersion = {
+  family: 'linux',
+  distro: 'ubuntu',
+  codename: 'focal',
+};
+
+export default hf;
--- test/example_plans/vanilla.mjs
+++ test/example_plans/vanilla.mjs
@@ -1,3 +1,10 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import sysFactsHelper from 'ubborg-sysfacts-helper-pmb';
+
+import hostFacts from './inc/vanilla.hostFacts';
+
 export default async(bun) => {
+  await sysFactsHelper(bun, hostFacts, true);
   bun.needs('bundle', 'cjs:ubborg-usecase-rescuedisk-pmb');
 };
