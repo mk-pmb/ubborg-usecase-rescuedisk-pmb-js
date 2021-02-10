24c740995af4c0cfb759c2e2fdb25661eea2b477
mk-pmb <mk-pmb@users.noreply.github.com>
2021-02-10 04:28:59 +0100

    v0.0.45: Workaround: Allow multiple pavucontrol on Ubuntu focal

---
+++ src/workarounds/focal/pavucontrolMultiInstance.mjs
@@ -0,0 +1,20 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+const explain = [
+  '# Workaround: Allow multiple instances even with pavucontrol 4.0-1build1',
+  '# from Ubuntu 20.04.2 LTS focal official repos.',
+];
+
+const noDBus = 'export DBUS_SESSION_BUS_ADDRESS=';
+const bpc = '/bin/pavucontrol';
+const fix = noDBus + '; exec /usr' + bpc + ' "$@"; exit $?';
+const wrapper = ['#!/bin/sh', fix, '#', explain, '#'];
+
+export default async(bun) => {
+  bun.needs('file', {
+    path: '/usr/local' + bpc,
+    enforcedModes: 'a=rx',
+    mimeType: 'lines',
+    content: [...wrapper, '# Installed via ' + bun.shortRelUrl()],
+  });
+};
