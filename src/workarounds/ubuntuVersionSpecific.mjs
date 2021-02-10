b87e628872f554e8c89b449550c9ce85f9379398
mk-pmb <mk-pmb@users.noreply.github.com>
2021-02-10 04:28:59 +0100

    v0.0.45: Workaround: Allow multiple pavucontrol on Ubuntu focal

---
+++ src/workarounds/ubuntuVersionSpecific.mjs
@@ -0,0 +1,10 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import sysFactsHelper from 'ubborg-sysfacts-helper-pmb';
+
+export default async(bun) => {
+  const osVer = await sysFactsHelper.mtd(bun, 'osVersion')();
+  const cn = (osVer || false).codename;
+  if (!cn) { return; }
+  await bun.needs('bundle', cn + '/');
+};
