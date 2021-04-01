a371c49c3e388425ff98b915c7473f6f29825138
mk-pmb <mk-pmb@users.noreply.github.com>
2021-04-02 00:24:06 +0200

    v0.0.53: Add cron

--- src/software/debPkg/sysCore.mjs
+++ src/software/debPkg/sysCore.mjs
@@ -17,6 +17,7 @@ export default async(bun) => {
     ...ubuMin,
     'anacron',
     'console-data',
+    'cron => cmd:crontab',
     // deprecated as of Ubuntu 18.04 // 'gksu',     // provides gksudo
     'initramfs-tools',
     'keyboard-configuration',
