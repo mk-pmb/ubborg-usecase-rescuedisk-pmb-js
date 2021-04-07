ad2de5a1468dd82a0bf7f13b02ec44e04cb57685
mk-pmb <mk-pmb@users.noreply.github.com>
2021-04-07 19:24:41 +0200

    v0.0.54: Ubuntu focal late modprobes

---
+++ src/workarounds/focal/lateModProbes.mjs
@@ -0,0 +1,28 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import sysdWants from 'ubborg-sysd-wants';
+
+const mods = [
+  'ax88179_178a',
+];
+
+export default async(bun) => {
+  const svcUnitSpec = {
+    mimeType: 'static_ini; speq',
+    pathPre: '/lib/systemd/system/',
+    path: 'ubborg-rescuedisk-late-modprobes',
+    pathSuf: '.service',
+    content: {
+      Service: {
+        SyslogIdentifier: '%N',
+        WorkingDirectory: '/',
+        Type: 'oneshot',
+        ExecStart: mods.map(m => 'modprobe ' + m),
+      },
+    },
+  };
+  await bun.needs('admFile', [
+    svcUnitSpec,
+    sysdWants('sysinit.target', true, svcUnitSpec),
+  ]);
+};
