b08026efd33240ca70748b08546aecc94bbda3e9
mk-pmb <mk-pmb@users.noreply.github.com>
2021-06-19 02:59:39 +0200

    Add Intel i965 VA driver and non-free shaders

---
+++ src/software/debPkg/driverVgaIntel.mjs
@@ -0,0 +1,10 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+const installProprietaryShaders = true;
+
+export default async(bun) => {
+  bun.needs('debPkg', [
+    'vainfo',
+    'i965-va-driver' + (installProprietaryShaders ? '-shaders' : ''),
+  ]);
+};
