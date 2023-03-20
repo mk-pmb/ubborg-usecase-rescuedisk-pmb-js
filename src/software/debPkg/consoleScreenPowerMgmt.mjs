71ea96268e3353e0eb559da2c221957c2fe1fd18
mk-pmb <mk-pmb@users.noreply.github.com>
2023-03-20 05:47:25 +0100

    Set up console screen power management

---
+++ src/software/debPkg/consoleScreenPowerMgmt.mjs
@@ -0,0 +1,28 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+const setTermScriptLines = [
+  '[ "$TERM" = linux ] || return 0',
+  '# Single dash arguments to setterm are for Ubuntu trusty compat.',
+  '# Ubuntu focal uses double dash arguments but still supports single.',
+  '',
+  '# Blank terminal after this many minutes of inactivity (0 = disable):',
+  'setterm -blank 5',
+  '',
+  '# Set display to VESA power saving mode…',
+  'setterm -powersave powerdown',
+  '# … after this many minutes of being blanked (0 = disable):',
+  'setterm -powerdown 5',
+];
+
+
+export default async (bun) => {
+  bun.needs('debPkg', [
+    'util-linux',   // provides setterm
+  ]);
+  bun.needs('admFile', [
+    { path: '/etc/profile.d/power_mgmt.console_screen.urd.sh',
+      mimeType: 'utf8_tw; 2; #',
+      content: setTermScriptLines,
+    },
+  ]);
+};
