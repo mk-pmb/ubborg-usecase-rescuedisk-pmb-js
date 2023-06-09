90dfe3406d1ffc7247f776b17aebb93620e654a2
mk-pmb <mk-pmb@users.noreply.github.com>
2023-06-10 01:14:22 +0200

    Avoid packages not available on jammy.

--- src/software/debPkg/remoteDesktop.mjs
+++ src/software/debPkg/remoteDesktop.mjs
@@ -1,20 +1,24 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-const remminaPlugins = [
-  'nx',
-  'rdp',
-  'secret',
-  'spice',
-  'vnc',
-  'www',
-  'xdmcp',
-].map(n => 'remmina-plugin-' + n);
+import sysFactsHelper from 'ubborg-sysfacts-helper-pmb';
 
 export default async (bun) => {
+  const osCn = (await sysFactsHelper.mtd(bun, 'osVersion')()).codename;
+
+  const remminaPlugins = [
+    ((osCn !== 'jammy') && 'nx'),
+    'rdp',
+    'secret',
+    'spice',
+    'vnc',
+    'www',
+    ((osCn !== 'jammy') && 'xdmcp'),
+  ].filter(Boolean).map(n => 'remmina-plugin-' + n);
+
   bun.needs('debPkg', [
     'x11vnc',
 
     'remmina',
     ...remminaPlugins,
-  ]);
+  ].filter(Boolean));
 };
