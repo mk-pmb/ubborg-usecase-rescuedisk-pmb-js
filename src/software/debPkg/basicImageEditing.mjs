ed7c2db761c6b5b621c5c642c3b7f9de55bc0137
mk-pmb <mk-pmb@users.noreply.github.com>
2023-06-10 03:06:12 +0200

    basicImageEditing: Drop mtpaint, use pinta instead.

--- src/software/debPkg/basicImageEditing.mjs
+++ src/software/debPkg/basicImageEditing.mjs
@@ -2,7 +2,13 @@
 
 export default async (bun) => {
   bun.needs('debPkg', [
-    'mtpaint',
     'imagemagick',
+
+    'pinta => cmd:',
+    // drawing: Cannot find how to zoom in Ubuntu focal's ancient version.
+    // gnome-paint: "the zoom feature has not yet been implemented."
+    // mtpaint: Cannot move selection content. Dev considers this a feature!
+    // mypaint: Cannot paint single pixels even with minimum brush size.
+    // xpaint: Crashes on focal when trying to move selection.
   ]);
 };
