e8c93c186f67fbf786b4efd576369f4498802b65
mk-pmb <mk-pmb@users.noreply.github.com>
2023-11-21 20:58:09 +0100

    htpasswd

--- src/software/debPkg/cryptoAndRandomness.mjs
+++ src/software/debPkg/cryptoAndRandomness.mjs
@@ -2,6 +2,7 @@
 
 export default async (bun) => {
   bun.needs('debPkg', [
+    'apache2-utils => cmd:htpasswd',
     'ent',
     'haveged',    // waste less time waiting for randomness pool affusion
     'libcrypt-ssleay-perl',
