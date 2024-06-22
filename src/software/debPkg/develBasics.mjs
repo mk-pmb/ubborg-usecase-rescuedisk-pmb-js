0a01421382ce2cf288c659cd5ab69baf73c65e22
mk-pmb <mk-pmb@users.noreply.github.com>
2024-06-22 02:13:14 +0200

    Add manpages-dev.

--- src/software/debPkg/develBasics.mjs
+++ src/software/debPkg/develBasics.mjs
@@ -3,6 +3,8 @@
 export default async (bun) => {
   bun.needs('debPkg', [
     'build-essential',
+    'manpages-dev', // man pages for C stdlib functions
+    'manpages', // basics about GNU/Linux, e.g. devices, /etc/passwd, wtmp
 
     'geany => cmd:',  // Text editor that can fold JSON containers
     'gucharmap => cmd:',  // View Unicode blocks, e.g. for RegExp ranges.
