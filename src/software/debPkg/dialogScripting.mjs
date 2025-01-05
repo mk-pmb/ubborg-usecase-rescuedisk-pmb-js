61d523131b32424cb6a2b06b6bb33272faa36666
mk-pmb <mk-pmb@users.noreply.github.com>
2025-01-05 18:46:54 +0100

    stalonetray

--- src/software/debPkg/dialogScripting.mjs
+++ src/software/debPkg/dialogScripting.mjs
@@ -4,6 +4,7 @@ export default async (bun) => {
   bun.needs('debPkg', [
     'alltray',
     'gxmessage',
+    'stalonetray',
     'x11-utils',  // xmessage
     'yad',        // a better zenity
     'zenity',
