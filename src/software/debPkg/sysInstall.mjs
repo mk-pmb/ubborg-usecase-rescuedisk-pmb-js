087be72cda1160acf87dfc66ecf6808d579f642b
mk-pmb <mk-pmb@users.noreply.github.com>
2025-01-29 01:01:34 +0100

    linux-image-lowlatency-hwe-*-edge

--- src/software/debPkg/sysInstall.mjs
+++ src/software/debPkg/sysInstall.mjs
@@ -1,7 +1,16 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import sysFactsHelper from 'ubborg-sysfacts-helper-pmb';
+import ubuntuVersionsTable from 'ubuntu-versions-table-pmb';
+
+
 export default async (bun) => {
+  const osCn = (await sysFactsHelper.mtd(bun, 'osVersion')()).codename;
+  const osRel = ubuntuVersionsTable.byCodename(osCn).release;
+
   bun.needs('debPkg', [
+    'kpatch',
+    'linux-image-lowlatency-hwe-' + osRel + '-edge',
     'multistrap',
   ]);
 };
