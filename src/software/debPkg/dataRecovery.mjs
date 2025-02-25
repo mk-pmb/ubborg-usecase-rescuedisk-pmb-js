4889bcade958ec225f85fbf904f07b306f7dc7d5
mk-pmb <mk-pmb@users.noreply.github.com>
2025-02-25 07:15:43 +0100

    testdisk/PhotoRec

--- src/software/debPkg/dataRecovery.mjs
+++ src/software/debPkg/dataRecovery.mjs
@@ -3,5 +3,6 @@
 export default async (bun) => {
   bun.needs('debPkg', [
     'gddrescue',    // regular CLI ddrescue. g is for GNU, not GUI.
+    'testdisk',     // includes PhotoRec, the multimedia file carver.
   ]);
 };
