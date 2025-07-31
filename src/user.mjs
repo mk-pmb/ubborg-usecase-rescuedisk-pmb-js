d62728650dd3fa3b474de092950f87cff30a873e
mk-pmb <mk-pmb@users.noreply.github.com>
2025-07-31 23:29:26 +0200

    [minor] Fix lint: Use explicit filename extensions in imports.

--- src/user.mjs
+++ src/user.mjs
@@ -1,7 +1,7 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 import objPop from 'objpop';
-import mustBe from 'typechecks-pmb/must-be';
+import mustBe from 'typechecks-pmb/must-be.js';
 import passwordHash from 'ansible-bogus-linux-pwhash';
 
 import pkgMeta from '../package.json';
