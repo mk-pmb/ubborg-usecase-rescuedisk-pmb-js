dd5507b00d34e57e854412ab272cb39e729899d8
mk-pmb <mk-pmb@users.noreply.github.com>
2025-12-06 20:34:32 +0100

    The debPkg for rEFInd installs too eagerly.

--- src/software/debPkg/bootloaders.mjs
+++ src/software/debPkg/bootloaders.mjs
@@ -8,5 +8,17 @@ export default async (bun) => {
     'grub-pc-bin',
 
     'efibootmgr => cmd:',
+
+    'refind¬ => cmd:refind-install', /*
+      Unfortunately, that package too eagerly asks annoying questions about
+      installing it to the ESP, and (on focal) there seems to be no package
+      that provides just `refind-install`.
+      With `DEBIAN_FRONTEND=noninteractive`, the default answer about the
+      ESP seems to be "yes, install it", and the log message
+      "Installing rEFInd to the ESP..." unfortunately doesn't clarify whether
+      that means to also make it the default. Which is a bit scary because in
+      interactive mode, the question included whether to adjust NVRAM entries.
+      For safe auto-install, the deb package seems to not be the way to go.
+      */
   ]);
 };
