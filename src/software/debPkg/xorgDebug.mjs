051922a14343916dc2b7f5ce502fd5d67096e8b0
mk-pmb <mk-pmb@users.noreply.github.com>
2026-02-15 23:28:25 +0100

    Screen metadata detectors.

--- src/software/debPkg/xorgDebug.mjs
+++ src/software/debPkg/xorgDebug.mjs
@@ -3,5 +3,11 @@
 export default async (bun) => {
   bun.needs('debPkg', [
     'gkbd-capplet => cmd:gkbd-keyboard-display', // visual kbd layout viewer
+
+
+    // Screen metadata detectors:
+    'ddcutil => cmd:',
+    'edid-decode => cmd:',
+    'read-edid => cmd:get-edid',
   ]);
 };
