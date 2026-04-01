a8813bd82fdb7cde0de6e6c732f4447411cbfa8b
mk-pmb <mk-pmb@users.noreply.github.com>
2026-04-01 22:15:40 +0200

    [test] Cleanup script: Also remove auto-patch backups and diffs.

--- test/clean.sh
+++ test/clean.sh
@@ -2,4 +2,17 @@
 # -*- coding: utf-8, tab-width: 2 -*-
 set -o pipefail -o errexit
 cd -- "$(readlink -m -- "$BASH_SOURCE"/..)"
-find example_plans/ -mount -maxdepth 5 -type f -name '*.tmp.plusText.txt' -delete
+
+FIND=(
+  example_plans/
+  -mount
+  -maxdepth 5
+  -type f
+  '(' -false
+    -o -name '*.tmp.plusText.diff'
+    -o -name '*.tmp.plusText.txt'
+    -o -name '*.want.plusText.txt.*.bak'
+    ')'
+  -delete
+  )
+find "${FIND[@]}"
