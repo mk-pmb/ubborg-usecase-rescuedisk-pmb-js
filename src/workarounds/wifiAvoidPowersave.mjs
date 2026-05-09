8e45007a28a3c72374ebbe90dcf56c74d66d25ee
mk-pmb <mk-pmb@users.noreply.github.com>
2026-05-09 19:49:18 +0200

    Add more redundancy for wifiAvoidPowersave.

--- src/workarounds/wifiAvoidPowersave.mjs
+++ src/workarounds/wifiAvoidPowersave.mjs
@@ -10,15 +10,34 @@ const powerMgtOpt = 'rtw_power_mgnt';
 
 export default async (bun) => {
   // see also: ../software/debPkg/netMgr.mjs
+
+  const ruleNameSlug = 'wifi-avoid-powersave-urd';
+  const modprobeRuleFile = '/etc/modprobe.d/' + ruleNameSlug + '.conf';
+  const udevRuleFile = '/etc/udev/rules.d/60-' + ruleNameSlug + '.rules';
+  const maybeRedundant = '# This rule might be redundant with ';
+
   bun.needs('admFile', {
-    path: '/etc/modprobe.d/wifi-avoid-powersave-urd.conf',
+    path: modprobeRuleFile,
     mimeType: 'lines',
     content: [
       fileGeneratedHint('# ', '\n'),
+      maybeRedundant + udevRuleFile,
       ...[
         '8188eu',
         '8723bs',
       ].map(chipset => `options ${chipset} ${powerMgtOpt}=0 rtw_enusbss=0`),
+    ],
+  });
+
+  bun.needs('admFile', {
+    path: udevRuleFile,
+    mimeType: 'lines',
+    content: [
+      fileGeneratedHint('# ', '\n'),
+      '# Device class 0xE0 = Wireless Controller (wifi, bluetooth, …)',
+      maybeRedundant + modprobeRuleFile,
+      'ACTION=="add", SUBSYSTEM=="usb", ATTR{bDeviceClass}=="e0", \\',
+      '  TEST=="power/control", ATTR{power/control}="on"',
     ],
   });
 };
