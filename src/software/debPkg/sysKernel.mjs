1e4d435d474a88878fe3ac3bb4ccda09fe777be2
mk-pmb <mk-pmb@users.noreply.github.com>
2026-05-09 21:40:13 +0200

    Merge bundle sysInstall into sysKernel.

--- src/software/debPkg/sysKernel.mjs
+++ src/software/debPkg/sysKernel.mjs
@@ -1,7 +1,15 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import sysFactsHelper from 'ubborg-sysfacts-helper-pmb';
+import ubuntuVersionsTable from 'ubuntu-versions-table-pmb';
+
+
 export default async (bun) => {
+  const osCn = (await sysFactsHelper.mtd(bun, 'osVersion')()).codename;
+  const osRel = ubuntuVersionsTable.byCodename(osCn).release;
+
   bun.needs('debPkg', [
-    'linux-image-lowlatency',
+    'kpatch',
+    'linux-image-lowlatency-hwe-' + osRel + '-edge',
   ]);
 };
